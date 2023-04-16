f7ba8f71dbd0bee8c7bda88bb43f3ee5cc63b887
Kunlapath Paengsa <kunlapath.paengsa@gmail.com>
2023-04-16 19:38:24 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿public class Solution
+{
+    public int CountOdds(int low, int high)
+     => (low % 2 != 0 && high % 2 != 0) ||
+        (low % 2 == 0 && high % 2 != 0) || 
+        (low % 2 != 0 && high % 2 == 0)
+     ? ((high - low) / 2) + 1 : (high - low) / 2;
+}
