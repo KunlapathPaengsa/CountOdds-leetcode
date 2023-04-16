dfc46daaf2fbcd40c5aeaafa8a7eb8e94945d6bf
Kunlapath Paengsa <kunlapath.paengsa@gmail.com>
2023-04-16 19:40:15 +0700

    version 1.1

--- Program.cs
+++ Program.cs
@@ -5,4 +5,19 @@
         (low % 2 == 0 && high % 2 != 0) || 
         (low % 2 != 0 && high % 2 == 0)
      ? ((high - low) / 2) + 1 : (high - low) / 2;
+
+    public int CountOdds1(int low, int high)
+    {
+        int count = 0;
+        for (int i = low; i <= high; i++)
+        {
+            if (i % 2 != 0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
 }
